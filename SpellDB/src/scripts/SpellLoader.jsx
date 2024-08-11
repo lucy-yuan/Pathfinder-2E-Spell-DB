195e36c2d3972e043c54c92397c17330392bef8e
happy-dino <github-shared.request453@passmail.net>
2024-08-11 12:06:59 -0400

    Split out spell overrides

--- SpellDB/src/scripts/SpellLoader.jsx
+++ SpellDB/src/scripts/SpellLoader.jsx
@@ -1,6 +1,10 @@
-﻿const spells = require("./../data/spells.json");
+﻿let spells = require("./../data/spells.json");
+const overrides = require("./../data/dino.json");
 const spellTypes = require("./../data/spellTypes.json");
 export function loadSpellData() {
-    
+    spells = spells.filter(function(x) { 
+        return !overrides.find(function(y) { return x.name == y.name});
+    });
+    spells = spells.concat(overrides);
     return { spells, spellTypes };
 };
